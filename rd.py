bf3c1261f6cd078805fed3dd5716162efa64eb69
Min Cheung <ilove52345234@gmail.com>
2020-01-05 20:52:23 +0800

    basic rd

---
+++ rd.py
@@ -0,0 +1,13 @@
+
+# %  求餘數 1000%1001 = 1
+data = []
+count = 0
+with open('reviews.txt', 'r') as f:
+    for line in f:
+        data.append(line)
+        count += 1 ## count = count + 1
+        if count % 1000 == 0:
+            print(len(data))
+print(data[32])
+print('----------------')
+print(data[646799])
